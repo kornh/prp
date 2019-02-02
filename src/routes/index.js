0d0264065ad0035e8801bfec2add947ade4db908
kornh <kornelius@hellwich.me>
2019-02-02 02:40:05 +0100

    Change: using pug for error site

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,8 +6,7 @@ var apiRouter = require('./api');
 router.use('/api', apiRouter);
 
 router.get('*', function (req, res, next) {
-	res.send("Error 404: Page not Found");
-
+	res.render('error', {message:"Page not found", error:{status:'Error 404',stack:""}});
 });
 
 module.exports = router;
